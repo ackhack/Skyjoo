c40e034c649cd3640886972c28d16f100805a8a0
ackhack <christoph.fuerbacher@gmx.de>
2022-10-02 00:53:59 +0200

    multi message support

--- Skyjoo/Activities/GameActivity.cs
+++ Skyjoo/Activities/GameActivity.cs
@@ -21,76 +21,83 @@ namespace Skyjoo
             base.OnCreate(bundle);
 
             SetContentView(Resource.Layout.waiting_layout);
-            DependencyClass.Client.HostMessageRecieved += Client_HostMessageRecieved;
+            DependencyClass.Client.HostMessageRecieved += ClientRecievedHostMessage;
             if (DependencyClass.Server != null)
             {
-                DependencyClass.Server.ReceivedMessage += Server_ReceivedMessage;
+                DependencyClass.Server.ReceivedMessage += ServerReceivedMessage;
                 initGameAsHost();
             }
         }
 
-        private void Server_ReceivedMessage(object sender, NetworkCommunication.Core.MessageReceivedEventArgs e)
+        private void ServerReceivedMessage(object sender, NetworkCommunication.Core.MessageReceivedEventArgs e)
         {
             var senderIp = e.Host.Address;
-            var clientMessage = JsonConvert.DeserializeObject<SkyjoClientMessage>(Encoding.UTF8.GetString(e.Message));
-            var content = (JContainer)clientMessage.Content;
-            System.Diagnostics.Debug.WriteLine("Server: " + clientMessage.Type);
-
-            switch (clientMessage.Type)
+            var clientMessages = JsonConvert.DeserializeObject<SkyjoClientMessage[]>("[" + Encoding.UTF8.GetString(e.Message).Replace("}{", "},{") + "]");
+            foreach (var clientMessage in clientMessages)
             {
-                case SkyjoClientMessageType.Login:
-                    //ignore as game has already started
-                    break;
-                case SkyjoClientMessageType.FieldUpdate:
-                    var clientFieldUpdateMessage = content.ToObject<SkyjoClientFieldUpdateMessage>();
-                    if (clientFieldUpdateMessage.Type == FieldUpdateType.ShuffleStack)
-                    {
-                        var shuffleMessage = JsonConvert.SerializeObject(new SkyjoHostMessage(SkyjoHostMessageType.PlayerLogout, new SkyjoHostShuffleNumberMessage(new Random().Next())));
-                        DependencyClass.Server.SendMessageToClients(shuffleMessage);
-                        return;
-                    }
-                    var hostFieldUpdateMessage = JsonConvert.SerializeObject(new SkyjoHostMessage(SkyjoHostMessageType.FieldUpdate, new SkyjoHostFieldUpdateMessage(clientFieldUpdateMessage.PlayerIndex, clientFieldUpdateMessage.FieldIndex, clientFieldUpdateMessage.Type)));
-                    DependencyClass.Server.SendMessageToClients(hostFieldUpdateMessage);
-                    break;
-                case SkyjoClientMessageType.Logout:
-                    var logoutMessage = JsonConvert.SerializeObject(new SkyjoHostMessage(SkyjoHostMessageType.PlayerLogout, new SkyjoHostPlayerLogoutMessage(senderIp)));
-                    DependencyClass.Server.SendMessageToClients(logoutMessage);
-                    break;
+                var content = (JContainer)clientMessage.Content;
+                System.Diagnostics.Debug.WriteLine("Server: " + clientMessage.Type);
+
+                switch (clientMessage.Type)
+                {
+                    case SkyjoClientMessageType.Login:
+                        //ignore as game has already started
+                        break;
+                    case SkyjoClientMessageType.FieldUpdate:
+                        var clientFieldUpdateMessage = content.ToObject<SkyjoClientFieldUpdateMessage>();
+                        if (clientFieldUpdateMessage.Type == FieldUpdateType.ShuffleStack)
+                        {
+                            var shuffleMessage = JsonConvert.SerializeObject(new SkyjoHostMessage(SkyjoHostMessageType.PlayerLogout, new SkyjoHostShuffleNumberMessage(new Random().Next())));
+                            DependencyClass.Server.SendMessageToClients(shuffleMessage);
+                            return;
+                        }
+                        System.Diagnostics.Debug.WriteLine("Server Player " + clientFieldUpdateMessage.PlayerIndex + " Field " + clientFieldUpdateMessage.FieldIndex);
+                        var hostFieldUpdateMessage = JsonConvert.SerializeObject(new SkyjoHostMessage(SkyjoHostMessageType.FieldUpdate, new SkyjoHostFieldUpdateMessage(clientFieldUpdateMessage.PlayerIndex, clientFieldUpdateMessage.FieldIndex, clientFieldUpdateMessage.Type)));
+                        DependencyClass.Server.SendMessageToClients(hostFieldUpdateMessage);
+                        break;
+                    case SkyjoClientMessageType.Logout:
+                        var logoutMessage = JsonConvert.SerializeObject(new SkyjoHostMessage(SkyjoHostMessageType.PlayerLogout, new SkyjoHostPlayerLogoutMessage(senderIp)));
+                        DependencyClass.Server.SendMessageToClients(logoutMessage);
+                        break;
+                }
             }
         }
 
-        private void Client_HostMessageRecieved(object sender, NetworkCommunication.Core.MessageReceivedEventArgs e)
+        private void ClientRecievedHostMessage(object sender, NetworkCommunication.Core.MessageReceivedEventArgs e)
         {
-            var str = Encoding.UTF8.GetString(e.Message);
-            var hostMessage = JsonConvert.DeserializeObject<SkyjoHostMessage>(str);
-            var content = (JContainer)hostMessage.Content;
-
-            switch (hostMessage.Type)
+            var hostMessages = JsonConvert.DeserializeObject<SkyjoHostMessage[]>("[" + Encoding.UTF8.GetString(e.Message).Replace("}{", "},{") + "]");
+            foreach (var hostMessage in hostMessages)
             {
-                case SkyjoHostMessageType.GameStart:
-                    RunOnUiThread(() =>
-                    {
-                        SetContentView(Resource.Layout.game_layout);
-                    });
-                    var gameStartMessage = content.ToObject<SkyjoHostGameStartMessage>();
-                    startGameAsClient(gameStartMessage.Info);
-                    break;
-                case SkyjoHostMessageType.FieldUpdate:
-                    var fieldUpdateMessage = content.ToObject<SkyjoHostFieldUpdateMessage>();
-                    board.HandleFieldUpdate(fieldUpdateMessage.PlayerIndex, fieldUpdateMessage.FieldIndex, fieldUpdateMessage.Type);
-                    break;
-                case SkyjoHostMessageType.ShuffleNumber:
-                    var shuffleNumberMessage = content.ToObject<SkyjoHostShuffleNumberMessage>();
-                    board.HandleStackShuffle(shuffleNumberMessage.ShuffleNumber);
-                    break;
-                case SkyjoHostMessageType.PlayerLogout:
-                    var playerLogoutMessage = content.ToObject<SkyjoHostPlayerLogoutMessage>();
-                    board.HandlePlayerLogout(playerLogoutMessage.Ip);
-                    break;
-                case SkyjoHostMessageType.GameRestart:
-                    var restartMessage = content.ToObject<SkyjoHostGameRestartMessage>();
-                    board.HandleGameRestart(restartMessage.StackSeed);
-                    break;
+                var content = (JContainer)hostMessage.Content;
+
+                switch (hostMessage.Type)
+                {
+                    case SkyjoHostMessageType.GameStart:
+                        RunOnUiThread(() =>
+                        {
+                            SetContentView(Resource.Layout.game_layout);
+                        });
+                        var gameStartMessage = content.ToObject<SkyjoHostGameStartMessage>();
+                        startGameAsClient(gameStartMessage.Info);
+                        break;
+                    case SkyjoHostMessageType.FieldUpdate:
+                        var fieldUpdateMessage = content.ToObject<SkyjoHostFieldUpdateMessage>();
+                        System.Diagnostics.Debug.WriteLine("Client Player " + fieldUpdateMessage.PlayerIndex + " Field " + fieldUpdateMessage.FieldIndex);
+                        board.HandleFieldUpdate(fieldUpdateMessage.PlayerIndex, fieldUpdateMessage.FieldIndex, fieldUpdateMessage.Type);
+                        break;
+                    case SkyjoHostMessageType.ShuffleNumber:
+                        var shuffleNumberMessage = content.ToObject<SkyjoHostShuffleNumberMessage>();
+                        board.HandleStackShuffle(shuffleNumberMessage.ShuffleNumber);
+                        break;
+                    case SkyjoHostMessageType.PlayerLogout:
+                        var playerLogoutMessage = content.ToObject<SkyjoHostPlayerLogoutMessage>();
+                        board.HandlePlayerLogout(playerLogoutMessage.Ip);
+                        break;
+                    case SkyjoHostMessageType.GameRestart:
+                        var restartMessage = content.ToObject<SkyjoHostGameRestartMessage>();
+                        board.HandleGameRestart(restartMessage.StackSeed);
+                        break;
+                }
             }
         }
 
@@ -117,8 +124,9 @@ namespace Skyjoo
             {
                 if (value.Key.StartsWith(DependencyClass.LocalIp))
                 {
-                    board = new SkyjoBoard(this, info, i);
+                    board = new SkyjoBoard(this, i);
                     board.FieldUpdated += Board_FieldUpdated;
+                    board.InitGame(info);
                     break;
                 }
                 i++;
